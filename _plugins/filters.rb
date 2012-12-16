d2f0e4d2339a0e3f8cbefc9367b507f9d88b534d
Jean Carlo Emer <jeancarloemer@gmail.com>
2012-12-15 23:15:51 -0200

    Removing Jekyll date filter

--- _plugins/filters.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-	module Filters
-		def date_format(input)
-			months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-			months[input.strftime('%m').to_i] + ' de ' + input.strftime('%Y')
-		end
-	end
-end
