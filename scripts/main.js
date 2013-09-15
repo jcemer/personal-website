95ccbd6bbc320a85cd38b9d59bc92b307a11f7f2
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-09-15 18:34:35 -0300

    Normalize project code style (indentation, charset, ...)

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 // google analytics
 !function () {
-    var ga = document.createElement('script')
-      , s = document.getElementsByTagName('script')[0]
-    ga.type = 'text/javascript'
-    ga.async = true
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
-    s.parentNode.insertBefore(ga, s)
+  var ga = document.createElement('script')
+  var s = document.getElementsByTagName('script')[0]
+  ga.type = 'text/javascript'
+  ga.async = true
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
+  s.parentNode.insertBefore(ga, s)
 }()
