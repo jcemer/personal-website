77507104d6f5a133431b085e5f5b128a491654ab
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-09-15 10:26:11 -0300

    Remove script that makes links with rel='external' open on new window

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,3 @@
-
-!function () {
-    // extracted of Rye
-    function prefix(key, obj) {
-        var result
-          , upcased = key[0].toUpperCase() + key.substring(1)
-          , prefixes = ['moz', 'webkit', 'ms', 'o']
-
-        obj = obj || window
-
-        if (result = obj[key]){
-            return result
-        }
-
-        while(prefix = prefixes.shift()){
-            if (result = obj[prefix + upcased]){
-                break;
-            }
-        }
-        return result
-    }
-
-    // external links
-    document.addEventListener('click', function (e) {
-        var el = e.target
-          , sel = prefix('matchesSelector', el)
-        if (sel && sel.call(el, '[rel=external]')) {
-            e.preventDefault()
-            window.open(el.href)
-        }
-    })
-}()
-
 // google analytics
 !function () {
     var ga = document.createElement('script')
