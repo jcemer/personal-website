99d857671b31d25d26fb9de54d9be25fc6ed1ddc
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-09-16 00:46:01 -0300

    Add target blank to external links

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+//
+[].slice.call(document.getElementsByTagName('a')).forEach(function(element) {
+  if(element.href.indexOf(document.location.host) == -1) {
+    element.target = '_blank';
+  }
+})
+
 // google analytics
 !function () {
   var ga = document.createElement('script')
