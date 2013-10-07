c4f3b2f48d6946c90401d05c5f91ebe2bc72b6fc
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-10-06 21:44:31 -0300

    New layout

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,36 @@
   }
 })
 
+// menu
+!function () {
+  var element = document.getElementById('menu-toggle')
+  function toggle(event) {
+    element.href = (document.location.hash == '#menu') ? '#' : '#menu'
+    return toggle
+  }
+  window.addEventListener('hashchange', toggle())
+}()
+
 // google analytics
 !function () {
   var ga = document.createElement('script')
   var s = document.getElementsByTagName('script')[0]
   ga.type = 'text/javascript'
   ga.async = true
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
+  ga.src = 'http://www.google-analytics.com/ga.js'
   s.parentNode.insertBefore(ga, s)
+}()
+
+// disqus
+!function () {
+  if (!document.getElementById('disqus_thread')) {
+    return
+  }
+  var disqus_shortname = 'jeancarloemer'
+  var dsq = document.createElement('script')
+  var s = document.getElementsByTagName('script')[0]
+  dsq.type = 'text/javascript'
+  dsq.async = true;
+  dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+  s.parentNode.insertBefore(dsq, s)
 }()
