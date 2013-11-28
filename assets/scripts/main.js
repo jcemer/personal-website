f3d00d976481ba0a9650e7c97701a774fa888537
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-11-28 00:41:35 -0200

    Add ice cream mode

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,27 @@
   }
 })
 
+//
+!function () {
+  var kpr = [],
+      kcw = ['38', '38', '40', '40', '37', '39', '37', '39', '66', '65'];
+
+  function fn(k) {
+    if (kcw[kpr.length] && kcw[kpr.length] == k) {
+      kpr.push(k)
+      if (kpr.length === kcw.length) {
+        document.body.classList.add('ice-cream')
+      }
+    } else {
+      kpr.length = 0
+    }
+  }
+
+  window.addEventListener('keyup', function(event) {
+    fn(event.keyCode.toString());
+  })
+}()
+
 // menu
 !function () {
   var element = document.getElementById('menu-toggle')
